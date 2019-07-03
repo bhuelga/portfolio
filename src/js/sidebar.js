faa5649d4c56600876ce8e9cd3baa5b90466ce6e
Brandon Huelga <bahuelga@gmail.com>
2019-07-02 20:29:03 -0600

    added video, about, and contact

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -11,20 +11,26 @@ class Sidebar {
         $('#sidebar').removeClass('fixed');
         $('#sidebar ul li').removeClass('active');
         $('#sidebar ul li:eq(0)').addClass('active');
+        $('#sidebar img').attr('src', '../img/seal.png');
       }
 
+      // active links on scroll
       if (scrollTop > windowHeight && scrollTop < 2*windowHeight) {
         $('#sidebar ul li').removeClass('active');
         $('#sidebar ul li:eq(1)').addClass('active');
+        $('#sidebar > img').attr('src', '../img/turtle.png');
       } else if (scrollTop > 2*windowHeight && scrollTop < 3*windowHeight) {
         $('#sidebar ul li').removeClass('active');
         $('#sidebar ul li:eq(2)').addClass('active');
+        $('#sidebar > img').attr('src', '../img/tuna.png');
       } else if (scrollTop > 3*windowHeight && scrollTop < 4*windowHeight) {
         $('#sidebar ul li').removeClass('active');
         $('#sidebar ul li:eq(3)').addClass('active');
+        $('#sidebar > img').attr('src', '../img/shark.png');
       } else if (scrollTop > 4*windowHeight) {
         $('#sidebar ul li').removeClass('active');
         $('#sidebar ul li:eq(4)').addClass('active');
+        $('#sidebar > img').attr('src', '../img/octopus.png');
       }
     })
 
