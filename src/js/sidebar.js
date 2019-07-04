8fc438e98eac447621036649657c93125ed6786b
Brandon Huelga <bahuelga@gmail.com>
2019-07-04 13:48:39 -0600

    Changed title picture and added content

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,37 +1,10 @@
 class Sidebar {
   constructor() {
-    
-    // fix sidebar on scroll
-    $(window).scroll(function() {
-      const scrollTop = $(window).scrollTop();
-      const windowHeight = $(window).height();
-      if (scrollTop > windowHeight) {
-        $('#sidebar').addClass('fixed');
-      } else {
-        $('#sidebar').removeClass('fixed');
-        $('#sidebar ul li').removeClass('active');
-        $('#sidebar ul li:eq(0)').addClass('active');
-        $('#sidebar img').attr('src', '../img/seal.png');
-      }
+    this._changeSidebar();
 
-      // active links on scroll
-      if (scrollTop >= windowHeight && scrollTop < 2*windowHeight) {
-        $('#sidebar ul li').removeClass('active');
-        $('#sidebar ul li:eq(1)').addClass('active');
-        $('#sidebar > img').attr('src', '../img/turtle.png');
-      } else if (scrollTop >= 2*windowHeight && scrollTop < 3*windowHeight) {
-        $('#sidebar ul li').removeClass('active');
-        $('#sidebar ul li:eq(2)').addClass('active');
-        $('#sidebar > img').attr('src', '../img/tuna.png');
-      } else if (scrollTop >= 3*windowHeight && scrollTop < 4*windowHeight) {
-        $('#sidebar ul li').removeClass('active');
-        $('#sidebar ul li:eq(3)').addClass('active');
-        $('#sidebar > img').attr('src', '../img/shark.png');
-      } else if (scrollTop >= 4*windowHeight) {
-        $('#sidebar ul li').removeClass('active');
-        $('#sidebar ul li:eq(4)').addClass('active');
-        $('#sidebar > img').attr('src', '../img/octopus.png');
-      }
+    // fix sidebar on scroll
+    $(window).scroll(() => {
+     this._changeSidebar();
     })
 
     // sidebar links
@@ -43,5 +16,37 @@ class Sidebar {
         scrollTop: index * $(window).height()
       }, 300);
     })
+  }
+
+  _changeSidebar() {
+    const scrollTop = $(window).scrollTop();
+    const windowHeight = $(window).height();
+    if (scrollTop > windowHeight) {
+      $('#sidebar').addClass('fixed');
+    } else {
+      $('#sidebar').removeClass('fixed');
+      $('#sidebar ul li').removeClass('active');
+      $('#sidebar ul li:eq(0)').addClass('active');
+      $('#sidebar > img').attr('src', '../img/seal.png');
+    }
+
+    // active links on scroll
+    if (scrollTop >= windowHeight && scrollTop < 2*windowHeight) {
+      $('#sidebar ul li').removeClass('active');
+      $('#sidebar ul li:eq(1)').addClass('active');
+      $('#sidebar > img').attr('src', '../img/turtle.png');
+    } else if (scrollTop >= 2*windowHeight && scrollTop < 3*windowHeight) {
+      $('#sidebar ul li').removeClass('active');
+      $('#sidebar ul li:eq(2)').addClass('active');
+      $('#sidebar > img').attr('src', '../img/tuna.png');
+    } else if (scrollTop >= 3*windowHeight && scrollTop < 4*windowHeight) {
+      $('#sidebar ul li').removeClass('active');
+      $('#sidebar ul li:eq(3)').addClass('active');
+      $('#sidebar > img').attr('src', '../img/shark.png');
+    } else if (scrollTop >= 4*windowHeight) {
+      $('#sidebar ul li').removeClass('active');
+      $('#sidebar ul li:eq(4)').addClass('active');
+      $('#sidebar > img').attr('src', '../img/octopus.png');
+    }
   }
 }
